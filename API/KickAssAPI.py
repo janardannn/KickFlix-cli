3b15745fb95a852e7de6b7d2e3b208ce6771cafc
jh <jonardonhazarika7@gmail.com>
2021-08-21 17:53:07 +0530

    updated API to prevent IP ban and connection errors

--- API/KickAssAPI.py
+++ API/KickAssAPI.py
@@ -3,12 +3,15 @@ from bs4 import BeautifulSoup
 
 class KickAssAPI():
 
-    def search(query) -> dict:
+    def __init__(self):
+        self.headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'}
+
+    def search(self,query) -> dict:
 
         """ Returns a list of dictionaries with the results of the search """
 
         url = "https://katcr.to/usearch/" + query + "/"
-        results = requests.get(url)
+        results = requests.get(url, headers=self.headers)
 
         if results.status_code == 200:
             soup = BeautifulSoup(results.text, "html.parser")
@@ -17,18 +20,18 @@ class KickAssAPI():
             print("\nThe URL (\"https://katcr.to\") responded with code 403.\nThis means that the server understood the request but refuses to authorize it.")
             exit()
         elif results.status_code == 404:
-            print("\nThe URL (\"https://katcr.to\") responded with code 404.\nThis means that the server cannot find the page you requested.")
+            print("\nThe URL (\"https://katcr.to\") responded with code 404.\nThis means that the server cannot find the page you requested. ")
             exit()
         else:
             print("\nThe URL (\"https://katcr.to\") responded with code " + str(results.status_code) + ".\nThis means that the server is not responding to the request.")
         return results
 
 
-    def magnet(search_dict,index=0) -> str:
+    def magnet(self,search_dict,index=0) -> str:
 
         """ Returns the magnet link of the selected torrent """
 
-        magnet_page = requests.get("https://katcr.to"+search_dict[index].get("href")) 
+        magnet_page = requests.get("https://katcr.to"+search_dict[index].get("href"), headers=self.headers) 
         magnet_page_bs = BeautifulSoup(magnet_page.text, "html.parser")
         magnet_link = magnet_page_bs.find("a", {"class": "kaGiantButton"}).get("href")
 
