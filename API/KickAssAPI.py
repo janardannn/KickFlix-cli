020897fd4fd7037c36b114970c5197ffec56d55a
jh <jonardonhazarika7@gmail.com>
2021-09-09 14:53:04 +0530

    added 'Connection: closed' header to prevent the connection forcibly closed error

--- API/KickAssAPI.py
+++ API/KickAssAPI.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 class KickAssAPI():
 
     def __init__(self):
-        self.headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'}
+        self.headers = {'Connection':'close','user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'}
 
     def search(self,query) -> dict:
 
